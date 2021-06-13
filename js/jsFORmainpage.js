fa749a2e1abef8b590c1b74b864339183634978b
Abhay dePratap <abhaydepratap@gmail.com>
2021-06-13 21:53:21 +0530

    collected all js for main page

---
+++ js/jsFORmainpage.js
@@ -0,0 +1,73 @@
+function openNav() {
+  document.getElementById("mySidenav1").style.width = "100%";
+  document.getElementById("main").style.display = "none";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav1").style.width = "0";
+  document.getElementById("main").style.display = "block";
+}
+
+function nightie() {
+  var toChange = "mainheader";
+  document.getElementById(toChange).style.backgroundColor = "inherit";
+  document.getElementById(toChange).style.color = "white";
+}
+
+function openCat(evt, catName){
+    var i, category,discipline,listContent, tablinks;
+    category = document.getElementsByClassName("category");
+    for (i = 0; i < category.length; i++){
+        category[i].style.display = 'none';
+    }
+    discipline = document.getElementsByClassName("discipline");
+    for (i = 0; i < discipline.length; i++){
+        discipline[i].style.display = 'none';
+    }
+    listContent = document.getElementsByClassName("listContent");
+  for (i = 0; i < listContent.length; i++) {
+    listContent[i].style.display = "none";
+  }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" activeBtn", "");
+    }
+    document.getElementById(catName).style.display = "block";
+    evt.currentTarget.className += " activeBtn";
+
+
+}
+function openDis(evt, disName){
+    var i, discipline, tablinks;
+    discipline = document.getElementsByClassName("discipline");
+    for (i = 0; i < discipline.length; i++){
+        discipline[i].style.display = 'none';
+    }
+    listContent = document.getElementsByClassName("listContent");
+  for (i = 0; i < listContent.length; i++) {
+    listContent[i].style.display = "none";
+  }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" activeBtn", "");
+    }
+    document.getElementById(disName).style.display = "block";
+    evt.currentTarget.className += " activeBtn";
+
+}
+function openMainList(evt, listName) {
+  var i, listContent, tablinks;
+  listContent = document.getElementsByClassName("listContent");
+  for (i = 0; i < listContent.length; i++) {
+    listContent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" activeBtn", "");
+  }
+  document.getElementById(listName).style.display = "block";
+  evt.currentTarget.className += " activeBtn";
+}
+
+
+
